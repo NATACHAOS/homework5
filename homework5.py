ee9a98989aced8035eec3ca280a6c03beb1afdaf
NATACHAOS <164019299+NATACHAOS@users.noreply.github.com>
2024-03-27 14:20:24 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,11 @@
+my_list=['apple', 'lemon', 'coconut', 'peach','cherry']
+print(my_list)
+print((my_list)[0],(my_list)[-1])
+print((my_list)[2:5])
+my_list[2]='pineapple'
+print(my_list)
+my_dict={'D':123, 'A':34}
+print(my_dict)
+print(my_dict['D'])
+my_dict['Alex']=1112
+print(my_dict)
